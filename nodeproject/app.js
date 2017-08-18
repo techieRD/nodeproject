34eb8a48af3b5e79e1fd2a1dc189f862c373fa7d
techierd <angiegardi@yahoo.com>
2017-08-18 05:08:22 +0000

    added bower to manage js and css and added gulp

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,20 @@
-var aloha = 'Aloha my friend';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.get('/', function(req, res){
+    res.send('Aloha World');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Aloha Routing');
+});
+
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port);
+});
---
+++ nodeproject/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function(){
+    return gulp.src(jsFiles)
+        .pipe(jshint());
+});
+
+gulp.task('inject', function(){
+    var wiredep = require('wiredep').stream;
+    var options = { 
+        bowerJson: require('./bower.json'),
+        directory: './bower_components',
+        ignorePath: '../../bower_components'
+        
+    }
+    
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
+});
