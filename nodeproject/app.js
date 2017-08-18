eebc057b88f06f968dc9a6dbe2fe99b7eec5bc70
techierd <angiegardi@yahoo.com>
2017-08-18 06:25:30 +0000

    installed ejs

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -4,13 +4,20 @@ var app = express();
 var port = process.env.PORT;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 
 
 app.get('/', function(req, res){
-    res.send('Aloha World');
+    // res.send('Aloha World');
+    res.render('index', { 
+        list: ['first val', '2nd val', '3rd val'],
+        nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+    });
+        
 });
 
 app.get('/routing', function(req, res){
