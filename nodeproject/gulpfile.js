61cfce650507f3f49c1915759e1cb04c02bc5846
techierd <angiegardi@yahoo.com>
2017-08-18 05:46:32 +0000

    installed nodemon

--- nodeproject/gulpfile.js
+++ nodeproject/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -10,6 +11,13 @@ gulp.task('style', function(){
 
 gulp.task('inject', function(){
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js']);
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+    
     var options = { 
         bowerJson: require('./bower.json'),
         directory: './bower_components',
@@ -19,5 +27,20 @@ gulp.task('inject', function(){
     
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+
+gulp.task('serve', ['style', 'inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        watch: jsFiles
+    }
+    return nodemon(options)
+        .on('restart', function(ev){
+            console.log('Restarting Server ...');
+        })
+    
 });
